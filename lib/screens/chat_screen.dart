cc50bdc0c20924656c1b3b939896dca9ac7bd4d2
fimich1 <38435351+fimich1@users.noreply.github.com>
2023-01-23 13:26:03 +0500

    строки справа - слева

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,6 +5,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 final _firestore = FirebaseFirestore.instance;
+final _auth = FirebaseAuth.instance;
+late Firebase loggedInUser;
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
@@ -15,8 +17,7 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final messageTextController = TextEditingController();
-  final _auth = FirebaseAuth.instance;
-  late Firebase loggedInUser;
+
   late String messageText;
 
   @override
@@ -34,12 +35,6 @@ class _ChatScreenState extends State<ChatScreen> {
       print(e);
     }
   }
-  // Future<void> getMessages () async {
-  //  final messages = await _firestore.collection('messages').get();
-  //  for (var message in messages.docs) {
-  //    print(message.data());
-  //  }
-  // }
 
   void messagesStream() async {
     await for (var snapshot in _firestore.collection('messages').snapshots()) {
@@ -93,7 +88,7 @@ class _ChatScreenState extends State<ChatScreen> {
                       _firestore.collection('messages').add({
                         'text': messageText,
                         'sender': _auth.currentUser?.email, // это поменять?
-                            });
+                      });
                       //Implement send functionality.
                     },
                     child: Text(
@@ -116,7 +111,7 @@ class MessagesStream extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  StreamBuilder(
+    return StreamBuilder(
         stream: _firestore.collection('messages').snapshots(),
         builder: (context, snapshot) {
           List<MessageCub> messageCubs = [];
@@ -124,11 +119,18 @@ class MessagesStream extends StatelessWidget {
             final messages = snapshot.data?.docs;
             for (var message in messages!) {
               final messageText =
-              message.data()['text']; // this a firebase data
+                  message.data()['text']; // this a firebase data
               final messageSender = message.data()['sender'];
+              final currentUser = _auth.currentUser?.email;
+
+              if (currentUser == messageSender) {
+                //сообщение от меня
+              }
+
               final messageCub = MessageCub(
                 text: messageText,
                 sender: messageSender,
+                isMe: currentUser == messageSender,
               );
 
               messageCubs.add(messageCub);
@@ -142,42 +144,51 @@ class MessagesStream extends StatelessWidget {
           } //if
           return Expanded(
             child: ListView(
-                padding: EdgeInsets.symmetric(
-                    horizontal: 10.0, vertical: 20.0),
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
                 children: messageCubs),
           );
         } //builder
-    );
+        );
   }
 }
-
 
 class MessageCub extends StatelessWidget {
   //const MessageCub({Key? key}) : super(key: key);  -создалось автоматом
 
-  MessageCub({required this.text, required this.sender});
+  MessageCub({required this.text, required this.sender, required this.isMe});
   final String? sender;
   final String? text;
+  final bool isMe;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment:
+            isMe
+                ? CrossAxisAlignment.end
+                : CrossAxisAlignment.start,
+
+        //  crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Text(
             '$sender',
             style: TextStyle(
-            fontSize: 12.0,
+              fontSize: 12.0,
               color: Colors.black54,
-
             ),
           ),
           Material(
-            borderRadius: BorderRadius.circular(30.0),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              bottomLeft: Radius.circular(30.0),
+              bottomRight: Radius.circular(30.0),
+            ),
             elevation: 5.0,
-            color: Colors.lightBlueAccent,
+            color: isMe
+                ? Colors.lightBlueAccent
+                : Colors.white70,
             child: Padding(
               padding: EdgeInsets.symmetric(
                 vertical: 10.0,
@@ -185,7 +196,7 @@ class MessageCub extends StatelessWidget {
               ),
               child: Text('$text',
                   style: TextStyle(
-                    color: Colors.white70,
+                    color: Colors.blueGrey,
                     fontSize: 15.0,
                   )),
             ),
